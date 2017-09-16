897a9c55ac4cc6725a2d89ce50d312937c31537d
Jack Zhang <jack.tiger.zhang@gmail.com>
2017-09-16 02:53:48 -0700

    fixed errors where function calls to date without data gave an error

--- server/transaction_process_functions.py
+++ server/transaction_process_functions.py
@@ -3,8 +3,13 @@ import pandas as pd
 import datetime as dt
 
 def getFrequencies(tab, start_day, end_day, freq='daily'):
+    '''returns the frequencies of each category in the table tab from start_day to end_day based'''
+    date_range = [i for i in pd.date_range(start_day, end_day).values if i in tab.index.values]
+    out = create_categories_map(tab)
+    if (len(date_range)==0):
+        return out
     transaction_range = tab.loc[[i for i in pd.date_range(start_day, end_day).values if i in tab.index.values]]
-    out = df.groupby('Category')['Amount'].sum().to_dict()
+    out_val = df.groupby('Category')['Amount'].sum().to_dict()
     if (freq == 'weekly'):
         r = (end_day - start_day).days/7
     elif (freq=='monthly'):
@@ -14,12 +19,17 @@ def getFrequencies(tab, start_day, end_day, freq='daily'):
     else:
         r = (end_day - start_day).days
     for k in out:
-        out[k] = out[k]/r
+        out_val[k] = out_val[k]/r
+    for c in out_val:
+        out[c] = out_val[c]
     return out
 
 def getRangeExpenditure(tab, date_start, date_end, category=None):
     '''returns the amount of money spent in date range'''
-    today_transactions = tab.loc[[i for i in pd.date_range(date_start, date_end).values if i in tab.index.values]]
+    date_range = [i for i in pd.date_range(date_start, date_end).values if i in tab.index.values]
+    if (len(date_range)==0):
+        return 0
+    today_transactions = tab.loc[date_range]
     today_transactions.head()
     if (category==None):
         return sum(today_transactions['Amount'])
@@ -27,7 +37,9 @@ def getRangeExpenditure(tab, date_start, date_end, category=None):
         return sum(today_transactions[today_transactions['Category']==category]['Amount'])
 
 def getTodayExpenditure(tab, today=dt.datetime.today()):
-    '''gets amount of money spent today'''
+    '''gets amount of money spent today or the specified date'''
+    if (today not in tab.index):
+        return 0
     today_transactions = tab.loc[today.date()]
     return sum(today_transactions['Amount'])
 
@@ -39,3 +51,10 @@ def process_raw(tab):
     df['Date'] = pd.to_datetime(df['Date'])
     df = df.set_index(df['Date']).drop('Date',axis=1)
     return df
+
+def create_categories_map(tab, label='Category'):
+    cat_set = set(tab[label])
+    out = dict()
+    for c in cat_set:
+        out[c] = 0
+    return out

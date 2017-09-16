4665b6985af566568bcfc56d5ef2cfc5a1ffd138
Andrew <liboan1997@gmail.com>
2017-09-15 23:42:19 -0700

    takes both get and post

--- server/server.py
+++ server/server.py
@@ -1,6 +1,4 @@
-import json
-
-from flask import Flask, url_for, request, redirect
+from flask import Flask, url_for, request, redirect, json, jsonify
 
 
 app = Flask(__name__)
@@ -10,6 +8,18 @@ app = Flask(__name__)
 def root():
     return "Hello World"
 
+@app.route("/spending-today", methods=['GET'])
+def spending_today():
+    # TODO: Query CSV for total spending for today
+    resp = {'text': '200'}
+    return json.dumps(resp)
+
+@app.route('/set-goal', methods=['POST'])
+def set_goal():
+    print(request.values.get("goal"))
+    # TODO: Save goal value in storage (csv)
+
+    return str(request.values.get("goal"))
 
 if __name__ == "__main__":
     app.run(debug=True)
